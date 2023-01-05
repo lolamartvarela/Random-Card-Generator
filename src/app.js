da1dead4233653bb5f14f9cb72a4aacc49c5dc17
lolamartvarela <luciamvarela@outlook.com>
2023-01-05 19:47:37 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let palo = ["♦", "♥", "♠", "♣"];
+  let number = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  //Declaracion de arrays
+  let randPalo = Math.floor(Math.random() * palo.length);
+  let randNumber = Math.floor(Math.random() * number.length);
+  //radom
+  let upper = document.querySelector(".top");
+  let numbers = document.querySelector(".number");
+  let lower = document.querySelector(".bottom");
+  //obtencion de elementos
+
+  numbers.innerHTML = number[randNumber];
+
+  upper.innerHTML = palo[randPalo];
+  upper.style.color =
+    palo[randPalo] === "♥" || palo[randPalo] === "♦" ? "red" : "black";
+
+  lower.innerHTML = palo[randPalo];
+  lower.style.color =
+    palo[randPalo] === "♥" || palo[randPalo] === "♦" ? "red" : "black";
+  //manipulacion de elementos
 };
